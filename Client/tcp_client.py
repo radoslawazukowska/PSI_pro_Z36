df1872b6f7dfef79a4077d37add66b7dc8c04569
szczypawkaa <szczypawkaaa@gmail.com>
2026-01-17 05:04:48 +0100

    Refactor Client

--- Client/tcp_client.py
+++ Client/tcp_client.py
@@ -2,84 +2,64 @@ import socket
 import sys
 from time import sleep
 from message import Message, MessageType
+from dataclasses import dataclass
 
 
 HOST = "127.0.0.1"  # The server's hostname or IP addres
 PORT = 1234  # Port na ktorym nasłuchuje server
 
 
-def interactive_loop(sock, session):
-    while True:
-        print("1 - ClientHello")
-        print("2 - ServerHello")
-        print("3 - End")
-        print("4 - Message")
-        choice = input("> ")
+@dataclass
+class Client:
+    host: str
+    port: int
 
-        if choice == "1":
-            payload = session.public_key
-            msg = Message(MessageType.CLH, payload)
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            s.settimeout(0.5)
+            self.loop(s)
+        print("Client finished")
 
-        elif choice == "2":
-            payload = session.public_key
-            msg = Message(MessageType.SVH, payload)
+    def loop(self, sock):
+        while True:
+            cmd = input("client> ").strip()
 
-        elif choice == "3":
-            msg = Message(MessageType.END, b"")
+            # przed wykonaniem swojego zadania sprawdzam czy klient nie przerwał
+            try:
+                data = sock.recv(1024)
+                if len(data) > 0:
+                    msg = Message.from_bytes(data)
+                    if msg.type == MessageType.END:
+                        print("Operation not performed. Connection closed by server")
+                        break
+                    else:
+                        print("Received:", msg.body.decode("utf-8"))
 
-        elif choice == "4":
-            text = input("Message: ").encode("utf-8")
-            encrypted = session.encrypt_and_mac(text)
-            msg = Message(MessageType.MSG, encrypted)
+            except socket.timeout:
+                pass
 
-        else:
-            print("Invalid choice")
-            continue
+            if cmd == "CLH":
+                # Client hello
+                pass
 
-        sock.sendall(msg.encode())
+            elif cmd == "MSG":
+                cli_msg = Message(type=MessageType.MSG, body="Hello".encode("utf-8"))
+                sock.sendall(cli_msg.to_bytes())
 
-        if msg.msg_type == MessageType.END:
-            break
+            elif cmd == "END":
+                cli_msg = Message(type=MessageType.END, body=b"")
+                sock.sendall(cli_msg.to_bytes())
+
+                print("Connection closed by client")
+                break
+            else:
+                print("Commands: CLH | MSG | END")
 
 
 if __name__ == "__main__":
 
-    host = HOST
-    port = PORT
-    if len(sys.argv) < 3:
-        print("No host or port specfied")
-    else:
-        host = sys.argv[1]
-        port = int(sys.argv[2])
-
-    print(host)
-    print(port)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        s.settimeout(0.5)
-        while True:
-            body_str = input("Co napisać? ")
-
-            try:
-                data = s.recv(1024)
-                if len(data) > 0:
-                    msg = Message.from_bytes(data)
-                    print("Received:", msg.body.decode("utf-8"))
-                    if msg.type == MessageType.END:
-                        print("Connection closed by server")
-                        break
-            except socket.timeout:
-                pass
-
-            msg_type = MessageType.END if body_str == "end" else MessageType.MSG
-            body = b"" if msg_type == MessageType.END else body_str.encode("utf-8")
-
-            cli_msg = Message(type=msg_type, body=body)
-            s.sendall(cli_msg.to_bytes())
-
-            if msg_type == MessageType.END:
-                print("Connection closed by client")
-                break
-
-    print("Client finished")
+    host = sys.argv[1] if len(sys.argv) >= 3 else HOST
+    port = int(sys.argv[2]) if len(sys.argv) >= 3 else PORT
+    client = Client(host, port)
+    client.run()
