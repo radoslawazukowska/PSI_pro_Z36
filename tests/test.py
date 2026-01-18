d912d988a6db26acf9d78a9c4bf8aa6c16a526a0
radoslawazukowska <radoslawa.zuk@gmail.com>
2026-01-18 23:40:24 +0100

    script and output for testing

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,28 @@
 import hashlib
 
+
 if __name__ == "__main__":
-    with open("shared_key.bin", "rb") as f:
+    with open("shared_key_4.bin", "rb") as f:
         shared_key = f.read()
-    with open("hash.bin", "rb") as f:
+    with open("hash_4.bin", "rb") as f:
         mac_key = f.read()
-    with open("message.bin", "rb") as f:
+    with open("4_message.bin", "rb") as f:
         data = f.read()
     ciphertext = data[:-4]
     recv_mac = data[-4:]
     expected_mac = hashlib.sha256(mac_key + ciphertext).digest()[:4]
+
     if recv_mac != expected_mac:
         raise ValueError("MAC verification failed")
 
     key_len = len(shared_key)
     plaintext = bytes([b ^ shared_key[i % key_len] for i, b in enumerate(ciphertext)])
+
+    print("Zaszyfrowana wiadomość z MAC:")
+    print(" ".join(str(b) for b in data))
+
+    print("Zaszyfrowana wiadomość bez MAC:")
+    print(" ".join(str(b) for b in ciphertext))
+
+    print("Odszyfrowana wiadomość:")
     print(plaintext)
